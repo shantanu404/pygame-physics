a9198f2718ba224208538cf9b3ba24592796e982
shantanu69 <rahmanshantanu69@gmail.com>
2015-12-08 19:39:45 +0600

    Tutorial 3 - Adding Randomness

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import random
 import pygame
 import particle
+
 
 background_color = (255, 255, 255)
 (width, height) = (300, 200)
 
 def main():
 	screen = pygame.display.set_mode((width, height))
-	pygame.display.set_caption("Tutorial 2")
+	pygame.display.set_caption("Tutorial 3")
 	screen.fill(background_color)
 
-	myparticle = particle.Particle(screen, (150, 100), 15)
-	myparticle.display()
+	num_particles = 10;
+	my_particles = []
+
+	for n in range(num_particles):
+		size = random.randint(10, 20)
+		x = random.randint(size, width-size)
+		y = random.randint(size, height-size)
+		my_particles.append(particle.Particle(screen, (x, y), size))
+	for my_particle in my_particles:
+		my_particle.display()
 
 	pygame.display.flip()
 
