404de4565bb8153b11f8636dca730c704bd05f15
shantanu69 <rahmanshantanu69@gmail.com>
2015-12-08 19:16:57 +0600

    Tutorial 1 Creating a window

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+background_color = (255, 255, 255)
+(width, height) = (300, 200)
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Tutorial 1")
+screen.fill(background_color)
+
+pygame.display.flip()
+
+running = True
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
