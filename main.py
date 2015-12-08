0b9f430bc00bd3c602d3e8ef25eac31f0c67965f
shantanu69 <rahmanshantanu69@gmail.com>
2015-12-08 19:57:14 +0600

    Tutorial 4 - Random movement and speed

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import math
 import random
 import pygame
 import particle
 
 
 background_color = (255, 255, 255)
-(width, height) = (300, 200)
+(width, height) = (600, 400)
 
 def main():
 	screen = pygame.display.set_mode((width, height))
-	pygame.display.set_caption("Tutorial 3")
-	screen.fill(background_color)
+	pygame.display.set_caption("Tutorial 4")
 
 	num_particles = 10;
 	my_particles = []
@@ -18,17 +18,26 @@ def main():
 		size = random.randint(10, 20)
 		x = random.randint(size, width-size)
 		y = random.randint(size, height-size)
-		my_particles.append(particle.Particle(screen, (x, y), size))
-	for my_particle in my_particles:
-		my_particle.display()
 
-	pygame.display.flip()
+		my_particle = particle.Particle(screen, (x, y), size)
+		my_particle.speed = random.random()
+		particle.angle = random.uniform(0, math.pi*2)
+		
+		my_particles.append(my_particle)
 
 	running = True
 	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
+
+		screen.fill(background_color)
+
+		for my_particle in my_particles:
+			my_particle.move()
+			my_particle.display()
+		pygame.display.flip()
+
 
 if __name__ == '__main__':
 	main()
--- particle.py
+++ particle.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 class Particle(object):
 	"""docstring for Particle"""
@@ -6,8 +7,12 @@ class Particle(object):
 		(self.x, self.y) = pos
 		self.size = size
 		self.color = (10, 10, 10)
-		self.thickness = 2
+		self.thickness = 1
 		self.screen = screen
+		self.speed = 0.01
+		self.angle = math.pi/2
 	def display(self):
-		pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.size, self.thickness)
-		
+		pygame.draw.circle(self.screen, self.color, (int(self.x), int(self.y)), self.size, self.thickness)
+	def move(self):
+			self.x += (math.sin(self.angle) * self.speed)
+			self.y += (math.cos(self.angle) * self.speed)
